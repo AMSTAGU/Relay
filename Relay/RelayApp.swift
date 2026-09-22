234efe9972d1d14c5260155454112a4824076e4d
amstagu <amstagu@icloud.com>
2026-09-22 16:33:45 +0200

    Initial Xcode project

---
+++ Relay/RelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelayApp.swift
+//  Relay
+//
+//  Created by Amaury Gau on 22/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct RelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
